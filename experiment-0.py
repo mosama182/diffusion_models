bfa943ebb54f1e43bc78a7dfc75e460dea7fbe5c
mosama182 <m.osama182@gmail.com>
2024-12-03 16:00:17 +1100

    Adding DDIM reverse sampling for the toy example. Doing a sanity check to verify that the samples match the true distribution. However, a very large sigma2_q had to be used

--- experiment-0.py
+++ experiment-0.py
@@ -11,6 +11,10 @@
 # samples of x_{0}, then the histogram of these samples should match the true 
 # distribution N(mu_{0}, sigma^{2}_{0}). This should be true for any choice of
 # sigma^{2}_q and T too. 
+# Also added DDIM sampler which takes a deterministic step in the reverse sampler
+# The deterministic mapping is x_{t - \deltat} = x_{t} * 
+# \lambda (E[x_{t-\delta_t}| x_{t}] - x_{t}). The expectation is computed directly.
+# The score function can also be used to get the expectation
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,7 +31,7 @@ def score_function(x_t, t, mu_0, sigma2_0, sigma2_q):
     return score
 
 
-def reverse_sampler(mu_0, sigma2_0, sigma2_q, T):
+def reverse_sampler_ddpm(mu_0, sigma2_0, sigma2_q, T):
     """
     It generates a sample x0 by going through T steps in 
     the reverse order as in DDPM and using the above 
@@ -45,22 +49,71 @@ def reverse_sampler(mu_0, sigma2_0, sigma2_q, T):
     return x
 
 
+def expected_xt_deltat_given_xt(xt, mu_0, sigma2_0, sigma2_q, t, delta_t):
+    """
+    Computes the E[x_{t-\deltat}| x_{t}] for this toy problem
+    when p(x0) = N(mu_0, sigma2_0)
+    """
+    sigma2_t = sigma2_q * t
+    sigma2_t_deltat = sigma2_q * (t - delta_t)
+
+    expectation = ((sigma2_0 + sigma2_t_deltat) * xt + \
+                    (sigma2_q * delta_t * mu_0)) / (sigma2_0 + sigma2_t)
+
+    return expectation
+
+
+def reverse_sampler_ddim(mu_0, sigma2_0, sigma2_q, T):
+    """
+    It generates a sample x0 by going through T steps in 
+    the reverse order as in DDIM. 
+    Update: x_{t-\deltat} = x_{t} + \lambda_{t} * ()
+    E[x_{t-\deltat}|x_{t}] 
+    = x_{t} + \sigm2_q * \delta_{t} * \grad_{x_{t}} \log p_{t}(x_{t}) 
+     mu_0 and sigma2_0 are given as input
+    arguments because they are needed by the score function
+    """
+    delta_t = 1 / T
+    time_steps = np.linspace(1, delta_t, T)
+    x = np.random.normal(0, scale=np.sqrt(sigma2_q))
+    sigma_q = np.sqrt(sigma2_q)
+    lambda_step = lambda t: sigma_q * np.sqrt(t) / (sigma_q * np.sqrt(t - delta_t) + sigma_q * np.sqrt(t))
+    for t in time_steps:
+        # x = x + lambda * (E[x_{t-\deltat}| x_{t}] - x_{t})
+
+        # by explicity calculating the expectation
+        x = x + lambda_step(t) * (expected_xt_deltat_given_xt(x, mu_0, sigma2_0, sigma2_q, t, delta_t) - x)
+
+        # by using the score function to get the expectation
+        #x = x + lambda_step(t) * sigma2_q * delta_t * \
+        #    score_function(x, t, mu_0, sigma2_0, sigma2_q)
+
+    return x
+
+
 if __name__ == '__main__':
 
     # parameters of original normal distribution of x0
     mu_0, sigma2_0 = 3, 0.5
 
     # parameters of the reverse sampling process
-    sigma2_q = 10
+    sigma2_q = 50
     T = 1000
 
     # nos. of samples to obtain from reverse sampling
     nsamples = 1000
-    x0_samples = []
+    x0_samples_ddpm = []
+    x0_samples_ddim = []
 
     for i in range(nsamples):
-        x0 = reverse_sampler(mu_0, sigma2_0, sigma2_q, T)
-        x0_samples.append(x0)
+        
+        # use DDPM
+        x0 = reverse_sampler_ddpm(mu_0, sigma2_0, sigma2_q, T)
+        x0_samples_ddpm.append(x0)
+
+        # use DDIM
+        x0 = reverse_sampler_ddim(mu_0, sigma2_0, sigma2_q, T)
+        x0_samples_ddim.append(x0)
     
     
     # ========
@@ -75,15 +128,23 @@ if __name__ == '__main__':
     pxbase = 1/np.sqrt(2 * np.pi * sigma2_q) * np.exp(-(xbase_test)**2/(2 * sigma2_q))
 
     plt.figure()
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.plot(xbase_test, pxbase, label=r'p(x_{T})')
     plt.xlabel(r'x')
     plt.ylabel(r'p(x)')
     plt.grid()
     plt.legend()
 
-    plt.subplot(1, 2, 2)
-    plt.hist(x0_samples, density=True, label=r'Samples from DDPM')
+    plt.subplot(1, 3, 2)
+    plt.hist(x0_samples_ddpm, density=True, label=r'Samples from DDPM')
+    plt.plot(x0_test, px0, label=r'True p(x_{0})')
+    plt.xlabel(r'x')
+    plt.ylabel(r'p(x)')
+    plt.grid()
+    plt.legend()
+
+    plt.subplot(1, 3, 3)
+    plt.hist(x0_samples_ddim, density=True, label=r'Samples from DDIM')
     plt.plot(x0_test, px0, label=r'True p(x_{0})')
     plt.xlabel(r'x')
     plt.ylabel(r'p(x)')
