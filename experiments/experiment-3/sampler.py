72a72189a293db8b2e87f4ee9902dbd76aefda81
mosama182 <m.osama182@gmail.com>
2024-12-28 18:48:21 +1100

    Adjusting sampler to expect the model to predict E[xt_deltat|x_t] directly

--- experiments/experiment-3/sampler.py
+++ experiments/experiment-3/sampler.py
@@ -51,8 +51,19 @@ class DDIMSampler:
             device = next(model.parameters()).device
             xt_tensor = torch.tensor(xt, dtype=torch.float).to(device)
             t_tensor = torch.tensor(t, dtype=torch.float).reshape((1, )).to(device)
-            exp_x0_xt = model(xt_tensor, t_tensor)
-            exp_xt_deltat = (self.delta_t / t_tensor) * exp_x0_xt + (1 - self.delta_t / t_tensor) * xt_tensor
+
+            #################################
+            # Model estimates E[x_{0}| x_{t}]
+            #################################
+            #exp_x0_xt = model(xt_tensor, t_tensor)
+            #exp_xt_deltat = (self.delta_t / t_tensor) * exp_x0_xt + (1 - self.delta_t / t_tensor) * xt_tensor
+            
+            #######################################
+            # Model estimates E[x_{t}| x_{t_delta}]
+            #######################################
+            exp_xt_deltat = model(xt_tensor, t_tensor)
+
+            # convert to numpy
             exp_xt_deltat = exp_xt_deltat.numpy() # 1 x 2 numpy array
         
         scale = np.sqrt(t)/ (np.sqrt(t) + np.sqrt(t-self.delta_t))
