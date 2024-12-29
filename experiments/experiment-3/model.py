76c9ed80f3e68e54fca791f822e848455fa93e11
mosama182 <m.osama182@gmail.com>
2024-12-29 12:54:58 +1100

    Changing the time embedding

--- experiments/experiment-3/model.py
+++ experiments/experiment-3/model.py
@@ -7,9 +7,9 @@ class TimeInputMLP(nn.Module):
         
         # Three layers with ReLU activation
         # Adding 2 to num_input because time 
-        # will be mapped to a 2-dimensional embedding
+        # will be mapped to a 1-dimensional embedding
         layers = []
-        layers.extend([nn.Linear(num_input + 2, num_hidden), nn.ReLU()])
+        layers.extend([nn.Linear(num_input + 1, num_hidden), nn.ReLU()])
         layers.extend([nn.Linear(num_hidden, num_hidden), nn.ReLU()])
         layers.extend([nn.Linear(num_hidden, num_hidden), nn.ReLU()])
         num_output = num_input
@@ -20,8 +20,9 @@ class TimeInputMLP(nn.Module):
     def time_embed(self, t):
         # t has shape (batch_size, )
         t = t.unsqueeze(1)
-        time_embed = torch.cat([torch.cos(2 * torch.pi * t), torch.sin(2 * torch.pi * t)], dim=1)
-        
+        #time_embed = torch.cat([torch.cos(2 * torch.pi * t), torch.sin(2 * torch.pi * t)], dim=1)
+        time_embed = t
+
         return time_embed
     
     def forward(self, x, t):
@@ -29,9 +30,9 @@ class TimeInputMLP(nn.Module):
         x : tensor (batch_size, 2) # x_{t + \deltat}
         t: tensor (batch_size, )   # t + \deltat
         """
-        
+
         time_embed = self.time_embed(t) # (batch_size, 2)
-        model_input = torch.cat([x, time_embed], dim=1) # (batch_size, 4)
+        model_input = torch.cat([x, time_embed], dim=1) # (batch_size, 3)
 
         return self.net(model_input) # \hat{x_{t}}
     
