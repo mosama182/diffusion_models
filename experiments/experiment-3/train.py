6bbda67e25095e95b9997704b8c958a76c0e142a
mosama182 <m.osama182@gmail.com>
2025-01-01 17:26:33 +1100

    Adjusting script description to estimating E[x_0|x_t] instead o E[x_t | x_{t + \deltat}]

--- experiments/experiment-3/train.py
+++ experiments/experiment-3/train.py
@@ -11,18 +11,26 @@ is in R^{2}.
 #######################
 WHAT WE AIM TO ESTIMATE
 #######################
-The model will be trained to estimate E[x_t | x_{t + \delat}]. 
-We know that E[Y|X] is the MMSE estimator. Here Y = x_{t} and 
-X = [x_{t + \deltat}, t + \deltat].  
-So the input to the model will be x_{t + \deltat}, t + \deltat 
-and the output will \hat{x_t} and we will be minimizing the mean
-square error.
+The model will be trained to estimate E[x_{0} | x_{t}]. 
+We know that E[Y|X] is the MMSE estimator. Here Y = x_{0} and 
+X = [x_{t}, t].  
+So the input to the model will be x_{t}, t
+and the output will \hat{x_0} and we will be minimizing the mean
+square error. 
+
+**Note:** Originally, I wanted to estimate E[x_{t} | x_{t + \deltat}]
+          but was not successful in doing that. So I estimate
+          E[x_{0} | x_{t}] and use the relationship between  
+          E[x_{t} | x_{t + \deltat}] and E[x_{0} | x_{t}] to obtain
+          former from the later. 
+          **Todo** I will investigate later why I was failing to 
+          estimate the original quantity directly        
 
 #########################
 GETTING A TRAINING SAMPLE
 #########################
 Given a datapoint x_{0}, we need to produce a training sample 
-(x_{t}, x_{t + \deltat}, t + \deltat) and for this we will be
+(x_{t}, t) and for this we will be
 creating two functions/classes:
     1. Schedule: is a class that defines N fixed time points
     in interval [0, 1]. When generating a training sample, we call
@@ -34,19 +42,21 @@ creating two functions/classes:
         t = Schedule(x0)
         \eta_{t} ~ N (0, sigma2_q * t * I)
         x_{t}  = x_{0} + \eta_{t}
-        epsilon ~ N (0, \sigma2_q * \deltat) 
-        x_{t + \deltat} = x_{t} + epsilon  
+        
+        #epsilon ~ N (0, \sigma2_q * \deltat) -- ignored 
+        #x_{t + \deltat} = x_{t} + epsilon  -- ignored
 
 ########
 NN MODEL
 ########
 The model is simply a fully connected NN. It has 3 layers with 128 neurons per
-layer and ReLU activation. The raw input to the model x_{t+\deltat} and t+\deltat
-are converted to a 4-dimensional vector. The first 2 dimensions are simply the raw 
-x_{t + \deltat} itself and the next 2 dimensions is a transformed times embedding
-\phi(t+\delta) = [sin(2 * pi * (t + \deltat)), cos(2 * pi * (t + \deltat))]. So the 
-first fully connected layer has input dimension of 4. The output layer has dimenson 
-2, and estimated \hat{x_{t}}
+layer and ReLU activation. The raw input to the model x_{t} and t
+are converted to a 3-dimensional vector. The first 2 dimensions are simply the raw 
+x_{t} itself and the next 1 dimensions is a transformed times embedding
+\phi(t) = [t]. It can be something else as well e.g. 
+\phi(t) = [sin(2 * pi * (t)), cos(2 * pi * (t))]. 
+So the first fully connected layer has input dimension of 3 (in the later case 4). 
+The output layer has dimenson 2, and estimated \hat{x_{0}}
 
 """
 import os
